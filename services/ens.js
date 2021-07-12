d2888b3afe2fcadc98ad27d511d021400ef0b92e
Steven Kulesza <s.kulesza@calendly.com>
2021-07-12 13:38:40 -0400

    use logindex

--- services/ens.js
+++ services/ens.js
@@ -4,16 +4,15 @@ import { pool } from '../db/setup.js'
 import { API } from "../api/index.js"
 import { SMS } from "./sms.js"
 
+const api = new API()
+const sms = new SMS()
 export class ENS {
     async insertNames() {
-        const api = new API()
-        const sms = new SMS()
-        
         const names = await api.getENSNames().then(names => names)
-        const formattedNames = names['setENSNames'].map(name => ([
-            name.id,
-            name.name,
-            name.id.split('-')[0]
+        const formattedNames = names['setENSNames'].map(({id, name}) => ([
+            id,
+            name,
+            id.split('-')[0]
         ]))
         
         pool.query(format(insertEnsTable, formattedNames), (error, results) => {
--- src/mapping.ts
+++ src/mapping.ts
@@ -2,7 +2,7 @@ import { ENS, SetENSName } from "../generated/ENS/ENS"
 import { SetENSName as setName } from "../generated/schema"
 
 export function handleSetENSName(event: SetENSName): void {
-  let id = event.address.toHexString().concat('-').concat(event.params.name.toString())
+  let id = event.address.toHexString().concat('-').concat(event.logIndex.toString())
   let entity = new setName(id)
   entity.name = event.params.name
   entity.save()
