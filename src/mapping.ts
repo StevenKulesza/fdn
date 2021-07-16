8120055510b5acd768a729cef30d739ce52c208d
Steven Kulesza <s.kulesza@calendly.com>
2021-07-16 11:24:52 -0400

    (bug): update to transaction.from address

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,7 +2,7 @@ import { ENS, SetENSName } from "../generated/ENS/ENS"
 import { SetENSName as setName } from "../generated/schema"
 
 export function handleSetENSName(event: SetENSName): void {
-  let id = event.address.toHexString().concat('-').concat(event.logIndex.toString())
+  let id = event.transaction.from.toHexString().concat('-').concat(event.logIndex.toString())
   let entity = new setName(id)
   entity.name = event.params.name
   entity.save()
